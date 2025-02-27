e4fe59edecfd8bc85915a801bcb01c47d5b05e53
minht_d1ff1c1le <143114074+minhtran1015@users.noreply.github.com>
2025-02-27 15:09:26 +0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,8 @@
 import sys
 import os
+from fastapi import FastAPI
+from credentials.controllers import router as credentials_router
+from calendar.controllers import router as calendar_router
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
@@ -9,5 +12,5 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-app.include_router(credentials_router)
-app.include_router(profile_router)
+app.include_router(credentials_router, prefix="/api/credentials")
+app.include_router(calendar_router, prefix="/api/calendar")
