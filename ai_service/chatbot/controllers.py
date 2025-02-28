244eb04f532b0fc62bf95b988be160f5463dc046
Akapi895 <83953516+Akapi895@users.noreply.github.com>
2025-02-28 13:07:44 +0700

    chat

--- ai_service/chatbot/controllers.py
+++ ai_service/chatbot/controllers.py
@@ -6,6 +6,7 @@ from .services import get_chat_response
 router = APIRouter()
 
 class ChatRequest(BaseModel):
+    user_id: int
     prompt: str
 
 class ChatResponse(BaseModel):
@@ -15,7 +16,7 @@ class ChatResponse(BaseModel):
 @router.get("/api/ai/chat/response", response_model=ChatResponse)
 async def chat_response(chat_request: ChatRequest):
     try:
-        response = await get_chat_response(chat_request.prompt)
+        response = await get_chat_response(chat_request.user_id, chat_request.prompt)
         print(response)
         return {"status": "success", "response": response}
     except Exception as e:
--- ai_service/chatbot/services.py
+++ ai_service/chatbot/services.py
@@ -8,7 +8,7 @@ load_dotenv()
 instructions_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'instructions.json')
 print(instructions_path)
 
-async def get_chat_response(prompt: str) -> str:
+async def get_chat_response(user_id: int, prompt: str) -> str:
     with open(instructions_path, 'r', encoding='utf-8') as f:
         instructions = json.load(f)
     instruction = instructions.get('instruction_1')
