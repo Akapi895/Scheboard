e120ed8b1b0f23e2a4cb59ee64c67d8cc627ea77
minht_d1ff1c1le <143114074+minhtran1015@users.noreply.github.com>
2025-03-02 00:17:45 +0700

    chỉ cần chạy = 1 terminal

--- backend/main.py
+++ backend/main.py
@@ -3,21 +3,24 @@ import os
 from fastapi import FastAPI
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from credentials.controllers import router as credentials_router
+from backend.credentials.controllers import router as credentials_router
 from backend.profile.controllers import router as profile_router
-# from backend.chatbot.controllers import router as chatbot_router
 from backend.calendar.controllers import router as calendar_router
 from backend.dashboard.controllers import router as dashboard_router
 from backend.main_task.controllers import router as main_task_router
-from fastapi import FastAPI
+
+from ai_service.chatbot.controllers import router as chatbot_router 
+from ai_service.calendar.controllers import router as ai_calendar_router 
 
 app = FastAPI()
 
 app.include_router(credentials_router)
 app.include_router(profile_router)
-# app.include_router(chatbot_router)
 app.include_router(dashboard_router)
 app.include_router(main_task_router)
 app.include_router(calendar_router, prefix="/api/calendar")
 
+app.include_router(chatbot_router)
+app.include_router(ai_calendar_router)
