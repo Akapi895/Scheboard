16053f8a438614d81f3ea475b122852f2a4c154c
Akapi895 <83953516+Akapi895@users.noreply.github.com>
2025-03-04 20:23:20 +0700

    chatbot profile

--- backend/main.py
+++ backend/main.py
@@ -10,6 +10,10 @@ from backend.chatbot.controllers import router as chatbot_router
 from backend.calendar.controllers import router as calendar_router
 from backend.dashboard.controllers import router as dashboard_router
 from backend.main_task.controllers import router as main_task_router
+from ai_service.chatbot.controllers import router as chatbot_router_ai
+# from ai_service.main_task.controllers import router as main_task_router_ai
+from ai_service.calendar.controllers import router as calendar_router_ai
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -21,3 +25,16 @@ app.include_router(dashboard_router)
 app.include_router(main_task_router)
 app.include_router(calendar_router, prefix="/api/calendar")
 
+app.include_router(chatbot_router_ai)
+# app.include_router(main_task_router_ai)
+app.include_router(calendar_router_ai)
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Hoặc thay "*" bằng frontend URL như "http://localhost:3000"
+    allow_credentials=True,
+    allow_methods=["*"],  # Cho phép tất cả phương thức (GET, POST, OPTIONS, v.v.)
+    allow_headers=["*"],
+)
