ccc3c8daed10d3138109d74c6ae45b3c71c04d24
Akapi895 <83953516+Akapi895@users.noreply.github.com>
2025-03-04 21:10:57 +0700

    commit login logout

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Sidebar.css';
 
-const Sidebar = () => {
+const Sidebar = ({ onLogout }: { onLogout: () => void }) => {
+  const navigate = useNavigate();
+  
+  const handleLogout = () => {
+    onLogout();
+    navigate('/login');
+  };
+  
   return (
     <div className="sidebar-container">
       <div className="sidebar">
@@ -79,13 +86,13 @@ const Sidebar = () => {
                             <path d="M2 18C1.45 18 0.979167 17.8042 0.5875 17.4125C0.195833 17.0208 0 16.55 0 16V2C0 1.45 0.195833 0.979167 0.5875 0.5875C0.979167 0.195833 1.45 0 2 0H9V2H2V16H9V18H2ZM13 14L11.625 12.55L14.175 10H6V8H14.175L11.625 5.45L13 4L18 9L13 14Z" fill="#06094D"/>
                         </svg>                                                                                                                                                                                            
                     </span>
-                    <span className = "title">Logout</span>
+                    <span className = "title" onClick={handleLogout}>Logout</span>
                 </Link>
             </li>
         </ul>
+      </div>
     </div>
-</div>
-);
+  );
 };
 
 export default Sidebar;
